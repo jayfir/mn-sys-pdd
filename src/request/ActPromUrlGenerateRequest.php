6d6c9bad2e74c6e89de85b304b872d680da8fcdc
jayfir <chinhui@coderfarmer.com>
2018-05-07 11:46:01 +0800

    1

---
+++ src/request/ActPromUrlGenerateRequest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace jayfir\pdd\request;
+
+use jayfir\pdd\RequestCheckUtil;
+
+/**
+ * @filename ActPromUrlGenerateRequest.php
+ * 
+ * @encoding UTF-8
+ * @author jinhuiZhang <chinhui@coderfarmer.com>
+ * @datetime 2018-5-7 11:35:15
+ */
+class ActPromUrlGenerateRequest
+{
+
+    /**
+     * 链接类型（传1）
+     * @var type 
+     */
+    private $urlType;
+
+    /**
+     * 是否生成短链接，true--生成
+     * @var type 
+     */
+    private $generateShortUrl;
+
+    /**
+     * 是否生成手机短链接，true--生成
+     * @var type 
+     */
+    private $generateMobileShortUrl;
+
+    /**
+     * p_id列表，如：["60005_612"]
+     * @var type 
+     */
+    private $pidList;
+    private $apiParas = array();
+
+    public function getApiMethodName()
+    {
+        return "pdd.ddk.act.prom.url.generate";
+    }
+
+    public function getApiParas()
+    {
+        return $this->apiParas;
+    }
+
+    public function setUrlType($urlType)
+    {
+        $this->urlType = $urlType;
+        $this->apiParas['url_type'] = $urlType;
+    }
+
+    public function getUrlType()
+    {
+        return $this->urlType;
+    }
+
+    public function setGenerateShortUrl($generateShortUrl)
+    {
+        $this->generateShortUrl = $generateShortUrl;
+        $this->apiParas['generate_short_url'] = $generateShortUrl;
+    }
+
+    public function getGenerateShortUrl()
+    {
+        return $this->generateShortUrl;
+    }
+
+    public function setGenerateMobileShortUrl($generateMobileShortUrl)
+    {
+        $this->generateMobileShortUrl = $generateMobileShortUrl;
+        $this->apiParas['generate_mobile_short_url'] = $generateMobileShortUrl;
+    }
+
+    public function check()
+    {
+        RequestCheckUtil::checkNotNull($this->urlType, 'url_type');
+    }
+
+    public function putOtherTextParam($key, $value)
+    {
+        $this->apiParas[$key] = $value;
+        $this->$key = $value;
+    }
+
+}
